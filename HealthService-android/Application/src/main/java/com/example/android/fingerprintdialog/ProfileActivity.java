85d3a73fb2ab193dd851b97da02f6ccea3199cf4
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-03-17 22:52:02 +0100

    back BUTTON WORKS!!!

--- HealthService-android/Application/src/main/java/com/example/android/fingerprintdialog/ProfileActivity.java
+++ HealthService-android/Application/src/main/java/com/example/android/fingerprintdialog/ProfileActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.fingerprintdialog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_next_appointment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), DashboardActivity .class);
+        startActivityForResult(myIntent, 0);
+        return true;
+
     }
 }
