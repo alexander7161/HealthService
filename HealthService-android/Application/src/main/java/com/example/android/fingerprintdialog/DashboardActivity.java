e6d3a4e902eaffed5534ddd9e2aefdb7dc0fde79
Alexander (Alex) Davis <k1761218@kcl.ac.uk>
2018-03-17 16:57:38 +0100

    Changed Logo and added security features

--- HealthService-android/Application/src/main/java/com/example/android/fingerprintdialog/DashboardActivity.java
+++ HealthService-android/Application/src/main/java/com/example/android/fingerprintdialog/DashboardActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.fingerprintdialog;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,6 +28,15 @@ public class DashboardActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //        TextView nextAppointment = (TextView) findViewById(R.id.);
+//        nextAppointment.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//      });
     }
 
     @Override
