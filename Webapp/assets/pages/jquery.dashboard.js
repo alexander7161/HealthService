f77980e9b55b8191fd48c8fec2561906b67d1d1e
Unknown <isaac.illiano@libero.it>
2018-03-17 19:31:43 +0100

    Update

--- Webapp/assets/pages/jquery.dashboard.js
+++ Webapp/assets/pages/jquery.dashboard.js
@@ -61,39 +61,39 @@
             labelColor: '#fff'
         });
     },
-    
-    
+
+
+
     Dashboard1.prototype.init = function() {
 
         //creating bar chart
         var $barData  = [
-            { y: '2010', a: 75 },
-            { y: '2011', a: 42 },
-            { y: '2012', a: 75 },
-            { y: '2013', a: 38 },
-            { y: '2014', a: 19 },
-            { y: '2015', a: 93 }
+            { y: '2013', a: 12 },
+            { y: '2014', a: 7 },
+            { y: '2015', a: 10 },
+            { y: '2016', a: 38 },
+            { y: '2017', a: 20 },
+            { y: '2018', a: 40 }
         ];
         this.createBarChart('morris-bar-example', $barData, 'y', ['a'], ['Statistics'], ['#188ae2']);
 
         //create line chart
         var $data  = [
-            { y: '2008', a: 50, b: 0 },
-            { y: '2009', a: 75, b: 50 },
-            { y: '2010', a: 30, b: 80 },
-            { y: '2011', a: 50, b: 50 },
-            { y: '2012', a: 75, b: 10 },
-            { y: '2013', a: 50, b: 40 },
-            { y: '2014', a: 75, b: 50 },
-            { y: '2015', a: 100, b: 70 }
+            { y: '2011', a: 50, b: 0 },
+            { y: '2012', a: 75, b: 50 },
+            { y: '2013', a: 30, b: 80 },
+            { y: '2014', a: 50, b: 50 },
+            { y: '2015', a: 75, b: 10 },
+            { y: '2016', a: 50, b: 40 },
+            { y: '2017', a: 75, b: 50 },
+            { y: '2018', a: 100, b: 70 }
           ];
-        this.createLineChart('morris-line-example', $data, 'y', ['a','b'], ['Series A','Series B'],['0.9'],['#ffffff'],['#999999'], ['#10c469','#188ae2']);
 
         //creating donut chart
         var $donutData = [
-                {label: "Download Sales", value: 12},
-                {label: "In-Store Sales", value: 30},
-                {label: "Mail-Order Sales", value: 20}
+                {label: "Midwife", value: 12},
+                {label: "General Practicer", value: 20},
+                {label: "Plastic Surgent", value: 3}
             ];
         this.createDonutChart('morris-donut-example', $donutData, ['#ff8acc', '#5b69bc', "#35b8e0"]);
     },
@@ -101,7 +101,7 @@
     $.Dashboard1 = new Dashboard1, $.Dashboard1.Constructor = Dashboard1
 }(window.jQuery),
 
-//initializing 
+//initializing
 function($) {
     "use strict";
     $.Dashboard1.init();
