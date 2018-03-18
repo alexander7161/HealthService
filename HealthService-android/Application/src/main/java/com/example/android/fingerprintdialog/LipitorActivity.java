328621360e9dd5d92ed93a5046ee3a05c2884c51
JohnHouslay <john.houslay@kcl.ac.uk>
2018-03-18 03:04:56 +0100

    back BUTTON WORKS!!!

--- HealthService-android/Application/src/main/java/com/example/android/fingerprintdialog/LipitorActivity.java
+++ HealthService-android/Application/src/main/java/com/example/android/fingerprintdialog/LipitorActivity.java
@@ -3,11 +3,11 @@ package com.example.android.fingerprintdialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Prescriptions extends AppCompatActivity {
+public class LipitorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prescriptions);
+        setContentView(R.layout.activity_lipitor);
     }
 }
